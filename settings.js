41d85d56cc65a929c70fa15905c5b2ca281c475e
Octanexo <167690379+Octanexo@users.noreply.github.com>
2024-05-22 23:17:36 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,28 +21,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = [923140152133 '] //ur owner number
+global.ownernomer = "92327091598" //ur owner number2
+global.ownername = "NOT ASIM MUNEER " //ur owner name
+global.ytname = "YT: @IRONWILL786 " //ur yt chanel name
+global.socialm = "GitHub: Sadistic_xc" //ur github or insta name
+global.location = "Pakistan , Punjab ,Islamabad " //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.botname = "BLACK_VIGO V4"
+global.ownernumber = 923140152133 '
+global.ownername = 'NOT ASIM MUNEER '
+global.ownerNumber = ["923140152133@s.whatsapp.net"]
+global.ownerweb = "https://www.instagram.com/sadistic_xc?igsh=M2xreHJlNzQyZGI1"
+global.websitex = "https://youtube.com/@ironwill786?si=Hbro6sins5P2Tc3C"
+global.wagc = "https://chat.whatsapp.com/KCGNpk4Vs7EAO3F8NflXzF"
+global.themeemoji = '👽'
+global.wm = "PROPERTY."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
-global.prefa = ['','!','.','#','&']
+global.packname = "PROPERTY OF"
+global.author = "NOT ASIM MUNEER \n\n"
+global.creator = "923140152133@s.whatsapp.net"
+global.prefa = ['','!','.','#','/']
 global.sessionName = 'session'
 global.hituet = 0
 
